d894dcf2951d4826ca59a88410b5f2381303659b
Colin te Kempel <ctekempel@gmail.com>
2023-01-31 17:21:55 +1030

    Removing commented out code

--- src/Windows/DesktopDirector.Ui/MainPage.xaml.cs
+++ src/Windows/DesktopDirector.Ui/MainPage.xaml.cs
@@ -21,19 +21,15 @@ namespace DesktopDirector.Ui
 
             AvailablePluginDescriptors = new ObservableCollection<PluginDescriptor>(app.PluginDescriptors);
             availablePluginDescriptors.ItemsSource = AvailablePluginDescriptors;
-
-            //selectedComponentPlugins.ItemsSource = AvailablePlugins;
         }
 
 
         public ObservableCollection<ComponentConfiguration> Components { get; set; }
         public ObservableCollection<PluginDescriptor> AvailablePluginDescriptors { get; set; }
-        //public ComponentConfiguration SelectedComponentConfiguration { get; set; }
-        //public Plugin SelectedPlugin { get; set; }
+
 
         private void OnComponentsItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
-            //selectedPluginConfiguration.Text = string.Empty;
 
             var selectedComponent = components.SelectedItem as ComponentConfiguration;
 
@@ -44,22 +40,6 @@ namespace DesktopDirector.Ui
                 selectedComponentComponentType.Text = selectedComponent.Component.ComponentType;
 
                 selectedComponentConfiguredPlugins.ItemsSource = new ObservableCollection<PluginConfiguration>(selectedComponent.Plugins);
-                //    selectedComponentName.Text = selectedComponent.Component.Name;
-                //    selectedComponentAddress.Text = selectedComponent.Component.Address;
-                //    selectedComponentType.Text = selectedComponent.Component.ComponentType;
-
-                //    if(selectedComponent.Plugins != null && selectedComponent.Plugins.Any())
-                //    {
-                //        var plugin = selectedComponent.Plugins.First();
-
-                //        selectedComponentPlugins.SelectedItem = plugin.Plugin.Name;
-                //        selectedPluginConfiguration.Text = plugin.Configuration;
-                //    }
-                //    else
-                //    {
-                //        selectedComponentPlugins.SelectedItem = null;
-                //        selectedPluginConfiguration.Text= string.Empty;
-                //    }
             }
         }
         private void OnAddPluginClicked(object sender, EventArgs e)
@@ -78,35 +58,6 @@ namespace DesktopDirector.Ui
 
             }
         }
-
-
-
-        private void OnSelectedComponentPluginsSelectedItemChanged(object sender, EventArgs e)
-        {
-            //if (SelectedComponentConfiguration != null)
-            //{
-            //    var plugin = app.Plugins.First(plugin => plugin.Name == (string)selectedComponentPlugins.SelectedItem);
-            //    SelectedPlugin = plugin;
-            //}
-            //else
-            //{
-
-            //}
-
-            //var selectedComponent = components.SelectedItem as ComponentConfiguration;
-
-            //if(selectedComponentPlugins.SelectedItem!= null) 
-            //{
-            //    var plugin = app.Plugins.First(plugin => plugin.Name == (string)selectedComponentPlugins.SelectedItem);
-            //    selectedComponent.Plugins = new PluginConfiguration[] { new PluginConfiguration { Plugin = plugin } };
-            //}
-            //else
-            //{
-            //    selectedComponent.Plugins = null;
-            //}
-        }
-
-
 
         private void OnAdditionalPluginSelected(object sender, EventArgs e)
         {
